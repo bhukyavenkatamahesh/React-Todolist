847e1adafd0563a44afcf03fb477c1700df77d01
Venkata Mahesh <bhukyavenkatamahesh@gmail.com>
2023-12-20 18:19:33 +0530

    Created a React Todo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newTask = { title, desc };
+    setTasks([...tasks, newTask]);
+    setTitle("");
+    setDesc("");
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white w-96 p-8 rounded shadow-md">
+        <h1 className="text-3xl font-bold mb-6 text-center">Todo List</h1>
+        <form onSubmit={submitHandler} className="mb-8">
+          <input
+            className="w-full border p-2 mb-2"
+            type="text"
+            placeholder="Enter the Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            className="w-full border p-2 mb-4"
+            type="text"
+            placeholder="Enter the Description"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+          >
+            Add Task
+          </button>
+        </form>
+        <ul>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="flex justify-between items-center bg-gray-200 p-2 mb-2 rounded"
+            >
+              <div>
+                <strong>{task.title}:</strong> {task.desc}
+              </div>
+              <button
+                onClick={() => deleteTask(index)}
+                className="text-red-500 hover:text-red-700"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
